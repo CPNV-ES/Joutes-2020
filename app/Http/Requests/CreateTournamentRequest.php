ab52d0c827b4c5a7c4f5bae7c98ebac8c37b658f
dlmw <12473240+dlmw@users.noreply.github.com>
2020-03-20 12:03:32 +0100

    Fix tournament creation validation

--- app/Http/Requests/CreateTournamentRequest.php
+++ app/Http/Requests/CreateTournamentRequest.php
@@ -28,7 +28,7 @@ class CreateTournamentRequest extends FormRequest
             'start_date' => 'required|date',
             'end_date' => 'required|date|after_or_equal:start_date',
             'sport_id' => 'required|exists:sports,id',
-            'max_teams' => 'required|min:2',
+            'max_teams' => 'required|numeric|min:2',
         ];
     }
 }
