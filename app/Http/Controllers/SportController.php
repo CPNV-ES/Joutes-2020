73f94d06e09927751107e7e8b4d59e79a1ed2e1a
Gabriel Pereira <54536712+gabpereira@users.noreply.github.com>
2020-05-01 10:47:07 +0200

    Add crud

---
+++ app/Http/Controllers/SportController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Sport;
+use Illuminate\Http\Request;
+
+class SportController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $sports = Sport::all();
+        return view('sports.index')->with(compact('sports'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $sports = Sport::all();
+        return view('sports.create')->with(compact('sports'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $sports = new Sport();
+
+        $sports->name = $request->input('name');
+        $sports->description = $request->input('description');
+        $sports->min_participant = $request->input('min_teams');
+        $sports->max_participant = $request->input('max_teams');
+
+        $sports->save();
+        //return view('sports.index')->with(compact('sports'));
+        return redirect()->route('sports.index', ['sports' => $sports]);
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $sports = Sport::all();
+        dd($sports);
+        return view('sports.edit')->with(compact('sport'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $sports = new Sport();
+
+        $sports->name = $request->input('name');
+        $sports->description = $request->input('description');
+        $sports->min_participant = $request->input('min_teams');
+        $sports->max_participant = $request->input('max_teams');
+
+        $sports->save();
+
+        return redirect()->route('sports.index', ['sports' => $sports]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $sports = Sport::find($id);
+        $sports->delete();
+
+        return redirect()->route('sports.index', ['sports' => $sports]);
+    }
+}
---
+++ resources/views/sports/edit.blade.php
@@ -0,0 +1,46 @@
+@extends('layout')
+
+@section('content')
+
+
+    <div class="row">
+        <div class="col-1 ml-n5">
+            <a href="{{ route('sports.index') }}"><i class="fa fa-4x fa-arrow-circle-left return fa-return growIcon" aria-hidden="true"></i></a>
+        </div>
+        <div class="col-11 ml-n2">
+            <h1>Sports</h1>
+
+            <hr>
+        </div>
+    </div>
+    <div class="container">
+
+        <div class="col-8">
+            <form action="" method="POST">
+                @csrf
+                <div class="form-group">
+                    <label for="tournamentName">Sports</label>
+                    <input type="text" class="form-control" id="name" name="name" value="{{$sports->name}}">
+                </div>
+
+                <div class="form-group">
+                    <label for="tournamentName">Description</label>
+                    <input type="text" class="form-control" id="description" name="description" value="{{$sports->description}}">
+                </div>
+
+                <div class="form-group row">
+                    <div class="col-6">
+                        <label for="start_date">Min de participant</label>
+                        <input type="number" class="form-control" id="min_teams" name="min_teams" min="1" max="99999999999" value="{{$sports->min_participant}}">
+                    </div>
+
+                    <div class="col-6">
+                        <label for="start_hour">Max de participant</label>
+                        <input type="number" class="form-control" id="max_teams" name="max_teams" min="1" max="99999999999" value="{{$sports->max_participant}}">
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-main">Edit</button>
+            </form>
+        </div>
+    </div>
+@stop
