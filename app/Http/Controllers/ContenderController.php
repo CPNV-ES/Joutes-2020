551043b437d1175e6b6fb081d5faceb1c6d151f5
dlmw <12473240+dlmw@users.noreply.github.com>
2020-05-06 15:18:35 +0200

    Add verification if contender (without team) already exists

--- app/Http/Controllers/ContenderController.php
+++ app/Http/Controllers/ContenderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Contender;
 use App\Pool;
+use Illuminate\Support\Facades\Redirect;
 
 class ContenderController extends Controller
 {
@@ -13,6 +14,13 @@ class ContenderController extends Controller
         $contender = new Contender();
         $contender->fill($request->all());
         $contender->pool()->associate($pool);
+
+        // if the team ID is not sent, it means we are editing a pool that is not in phase 1
+        // so it is required to check if the contender is already inserted
+        if (!isset($request->team_id) && $contender->alreadyInserted()) {
+            // TODO: redirect with an error message and show it to the user
+            return redirect()->back();
+        }
 
         $contender->save();
 
