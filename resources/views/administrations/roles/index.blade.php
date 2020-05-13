ab4a650fbd472bd8df12547a45c936cc345f64c4
Yvann <34337152+MrHD586@users.noreply.github.com>
2020-05-13 16:07:45 +0200

    Update index.blade.php

--- resources/views/administrations/roles/index.blade.php
+++ resources/views/administrations/roles/index.blade.php
@@ -55,7 +55,6 @@
                                      
                                     </div>
                                 </td>
-
                             </tr>
                         @endforeach
 
