1602b9d45aedaf8f32641bba57546a573aaf0659
Cyril Goldenschue <cyril.goldenschue@cpnv.ch>
2021-03-16 14:47:59 +0100

    Add poolState table on the database

--- database/migrations/2017_02_15_140645_create_pools_table.php
+++ database/migrations/2017_02_15_140645_create_pools_table.php
@@ -18,17 +18,18 @@ class CreatePoolsTable extends Migration
             $table->time('start_time')->nullable();
             $table->time('end_time')->nullable();
             $table->string('poolName', 45);
-            $table->integer('stage');
             $table->integer('poolSize');
-            $table->integer('isFinished');
+            $table->integer('stage');
 
             $table->integer('tournament_id')->unsigned();
             $table->integer('mode_id')->unsigned();
             $table->integer('game_type_id')->unsigned();
+            $table->integer('pool_states_id')->unsigned();
 
             $table->foreign('tournament_id')->references('id')->on('tournaments');
             $table->foreign('mode_id')->references('id')->on('pool_modes');
             $table->foreign('game_type_id')->references('id')->on('game_types');
+            $table->foreign('pool_states_id')->references('id')->on('pool_states');
         });
     }
 
