551757d7ba25880cb578faafea88bb405149ec27
Hausmann <william.hausmann@cpnv.ch>
2021-05-17 16:52:59 +0200

    Node modules for Data Table

--- webpack.mix.js
+++ webpack.mix.js
@@ -24,10 +24,19 @@ mix.js('resources/js/app.js', 'public/js')
     .copy('node_modules/bootstrap/dist/css/bootstrap.min.css', 'public/lib/bootstrap')
     .copy('node_modules/bootstrap/dist/js/bootstrap.min.js', 'public/lib/bootstrap')
 
+    //DataTable
+    .copy('node_modules/datatables.net/js/jquery.dataTables.js', 'public/lib/datatables/min/jquery.dataTables.min.js')
+    .copy('node_modules/datatables.net-bs4/js/dataTables.bootstrap4.min.js', 'public/lib/datatables/js/dataTables.bootstrap4.min.js')
+    .copy('node_modules/datatables.net-bs4/css/dataTables.bootstrap4.min.css', 'public/lib/datatables/css/dataTables.bootstrap4.min.css')
+    .copy('node_modules/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css', 'public/lib/datatables/css/dataTables.responsive.bootstrap4.min.css')
+    .copy('node_modules/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js', 'public/lib/datatables/js/dataTables.responsive.bootstrap4.min.js')
+    .copy('node_modules/datatables.net-responsive/js/dataTables.responsive.min.js', 'public/lib/datatables/js/dataTables.responsive.min.js')
+
     //font awesome
     .copyDirectory('node_modules/@fortawesome/fontawesome-free/css', 'public/lib/font-awesome/css')
     .copyDirectory('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/lib/font-awesome/webfonts')
 
     //sass
     .sass('resources/sass/main.scss', 'public/css');
+
 
