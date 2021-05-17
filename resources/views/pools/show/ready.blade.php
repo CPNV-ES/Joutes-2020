a3a650c487fbcecf7f3017c7c1eeb1bd902c38eb
zaidschouwey98 <zazar981@hotmail.com>
2021-05-17 13:41:32 +0200

    Add button

--- resources/views/pools/show/ready.blade.php
+++ resources/views/pools/show/ready.blade.php
@@ -19,7 +19,7 @@
                 
             <h2>Matches et Résultats</h2>
             <h4>État: {{ \App\Enums\PoolState::poolStateName($pool->poolState) }}</h4>
-            @if($pool->isEditable() && \App\Enums\EventState::eventStateName($pool->tournament->event->eventState) == 2)
+            @if($pool->isEditable() && \App\Enums\EventState::eventStateName($pool->tournament->event->eventState) == "En cours")
                 <button type="submit" class="btn btn-main" data-toggle="modal" data-target="#stagePoolModal">Passer à l'étape suivante</button>
             @endif
             <h4>Date : {{ $tournament->start_date->format('d.m.Y') }}</h4>
