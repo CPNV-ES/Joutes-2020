93b6bb59ac337df1d119e092ef0e4a7e4018cdc4
Cyril <cyril.goldenschue@cpnv.ch>
2021-05-27 09:13:47 +0200

    Add confirmation to change state of an event

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ Route::get('/', function () {
 Route::post('pools/contenders/unlink','ContenderController@detachContender');
 
 
-Route::get('/events/{event}/next_state',  'EventController@next_state')->name('events.next_state');
 
 Route::resource('events', 'EventController');
 Route::resource('tournaments', 'TournamentController');
@@ -31,7 +30,6 @@ Route::resource('tournaments.teams', 'TeamController');
 Route::resource('participants', 'ParticipantController');
 Route::resource('tournaments.pools', 'PoolController');
 Route::get('/tournaments/pools/{pool}', 'PoolController@close')->name('tournaments.pools.close');
-// Route::resource('contenders', 'ContenderController');
 Route::resource('pools.contenders', 'ContenderController');
 Route::resource('games', 'GameController');
 
