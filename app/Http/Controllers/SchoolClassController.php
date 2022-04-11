276023b562aca6c2420546eeb27d4b21ad8099ad
robielcpnv <robiel.tesfazghi@cpnv.ch>
2022-04-11 14:45:29 +0200

    Format SchoolClassController code

--- app/Http/Controllers/SchoolClassController.php
+++ app/Http/Controllers/SchoolClassController.php
@@ -16,29 +16,7 @@ class SchoolClassController extends Controller
     {
         $classes = SchoolClass::all();
 
-        $url = "https://intranet.cpnv.ch/sainte-croix/classes.json?api_key=demo&signature=deb6ccd76335e4c3e6a450d6c8db159d";
-
-        $curl = curl_init($url);
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-//for debug only!
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-
-        $resp = curl_exec($curl);
-        curl_close($curl);
-
-        $classes_array = json_decode($resp);
-        $classesIntranet = [];
-        foreach ($classes_array as $class) {
-            $classesIntranet[$class->name] = [
-                "name" => $class->name,
-                "year" => $class->moment->link->name,
-                "holder" => isset($class->master->link->name)?$class->master->link->name:'',
-                "delegate" => isset($class->representative->link->name) ? $class->representative->link->name : '',
-            ];
-        }
+        $classesIntranet = SchoolClass::fetchClassFromIntranet();
 
         return view('classes.index', compact('classes', 'classesIntranet'));
     }
--- app/SchoolClass.php
+++ app/SchoolClass.php
@@ -8,7 +8,34 @@ use Illuminate\Database\Eloquent\Model;
 class SchoolClass extends Model
 {
     use HasFactory;
+
     protected $fillable = ['class', 'status'];
 
+    public static function fetchClassFromIntranet()
+    {
+        $url = "https://intranet.cpnv.ch/sainte-croix/classes.json?api_key=demo&signature=deb6ccd76335e4c3e6a450d6c8db159d";
 
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+//for debug only!
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+        $resp = curl_exec($curl);
+        curl_close($curl);
+
+        $classes_array = json_decode($resp);
+        $classesIntranet = [];
+        foreach ($classes_array as $class) {
+            $classesIntranet[$class->name] = [
+                "name" => $class->name,
+                "year" => $class->moment->link->name,
+                "holder" => isset($class->master->link->name) ? $class->master->link->name : '',
+                "delegate" => isset($class->representative->link->name) ? $class->representative->link->name : '',
+            ];
+        }
+        return $classesIntranet;
+    }
 }
