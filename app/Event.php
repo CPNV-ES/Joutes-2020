f0312079b77f3c9622994fb1cdcf1637be3de42f
Noah <noah.delgado@cpnv.ch>
2022-05-16 13:49:21 +0200

    Add getExpectedParticipants & percentagParticipant

--- app/Event.php
+++ app/Event.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     public $timestamps = false;
-
+    public $percentageParticipation = 0;
     //Get event tournaments
     public function tournaments()
     {
@@ -43,5 +43,53 @@ class Event extends Model
     public function isRegisterOrReady()
     {
         return ($this->eventState == EventState::Register || $this->eventState == EventState::Ready);
+    }
+    /**
+     * Get percentage of participation related to this event from specific roles
+     *
+     * @return void
+     */
+    public function percentageParticipation()
+    {
+        $expectedPartipant = $this->getExpectedParticipants();
+        $actualParticipant = $this->users()
+            ->where('users.role_id', Role::findBySlug('PART')->id)
+            ->orWhere('users.role_id', Role::findBySlug('GEST')->id)
+            ->get()->unique();
+        return round((count($actualParticipant) / count($expectedPartipant)) * 100);
+    }
+
+    public function getExpectedParticipants()
+    {
+
+        $url = "https://intranet.cpnv.ch/sainte-croix/etudiants.json?alter[include]=current_class&api_key=demo&signature=49be40b0e7383ccd637804ad1faacfdf";
+
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        //for debug only!
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+        $resp = curl_exec($curl);
+        curl_close($curl);
+
+        $students_array = json_decode($resp);
+        $schoolClass = SchoolClass::all();
+
+        $studentsIntranet = [];
+        foreach ($students_array as $student) {
+            if ($schoolClass->contains('name', $student->current_class->name)) {
+                $studentsIntranet[$student->id] = [
+                    'firstname' => $student->firstname,
+                    //lowercase except first letter
+
+                    'lastname' =>  ucfirst(strtolower($student->lastname)),
+                    'class' => $student->current_class->name,
+                ];
+            }
+        }
+        return $studentsIntranet;
     }
 }
