15105cc490802508dde121cc50f5c9fd7fe1d208
Benoit <benoit.pierrehumbert.pro@gmail.com>
2023-05-15 15:36:00 +0200

    rename the new field to requierd

--- database/migrations/2023_05_15_132832_add_require_to_user_table.php
+++ database/migrations/2023_05_15_132832_add_require_to_user_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('require')->default(false)->after('password');
+            $table->boolean('required')->default(false)->after('password');
         });
     }
 
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->removeColumn('require');
+            $table->removeColumn('required');
         });
     }
 };
