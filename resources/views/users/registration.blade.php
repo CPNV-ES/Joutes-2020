ba81e9e51393e08679893b1ea896ea15616dbea9
Vivien Piccin <Vivien.Piccin@cpnv.ch>
2023-05-22 15:50:10 +0200

    Removed Hello World placeholder text

--- resources/views/users/registration.blade.php
+++ resources/views/users/registration.blade.php
@@ -1,24 +1,23 @@
 @extends('layout')
 @section('content')
-<h1>Hello World</h1>
-<table style="width: 100%;">
+<table style="width: 100%; border: solid gray 2px;">
     <thead>
         <tr>
-            <th colspan="3" style="text-align: center;">Unregistered required users</th>
+            <th colspan="3" style="text-align: center; border: solid gray 2px;">Unregistered required users</th>
         </tr>
     </thead>
     <tbody>
         @foreach($requiredUnregisteredUsers as $user)
         <tr>
-            <td>{{ $user->last_name }}</td>
-            <td>{{ $user->first_name }}</td>
-            <td><a href="mailto:{{$user->email}}">{{ $user->email }}</a></td>
+            <td style="border: solid gray 1px;">{{ $user->last_name }}</td>
+            <td style="border: solid gray 1px;">{{ $user->first_name }}</td>
+            <td style="border: solid gray 1px;"><a href="mailto:{{$user->email}}">{{ $user->email }}</a></td>
         </tr>
         @endforeach
     </tbody>
     <tfoot>
         <tr>
-            <th colspan="3" style="text-align: center;"><a href="mailto:?bcc=@foreach($requiredUnregisteredUsers as $user){{ $user->email }};@endforeach">Send email to all</a></th>
+            <th colspan="3" style="text-align: center; border: solid gray 2px;"><a href="mailto:?bcc=@foreach($requiredUnregisteredUsers as $user){{ $user->email }};@endforeach">Send email to all</a></th>
         <tr>
     </tfoot>
 </table>
