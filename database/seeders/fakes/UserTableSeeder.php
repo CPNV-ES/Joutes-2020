7ec0ba7ee1035926496ccf8113e5273134000a79
vicmtn <victorien.montavon@gmail.com>
2023-05-22 15:09:24 +0200

    update fake seeder

--- database/seeders/fakes/UserTableSeeder.php
+++ database/seeders/fakes/UserTableSeeder.php
@@ -29,7 +29,7 @@ class UserTableSeeder extends Seeder
             DB::table('team_users')->insert([
                 'user_id' => $index,
                 'team_id' => rand(1, 68),
-                'isCaptain' => $faker->boolean
+                'isCaptain' => 0,
             ]);
         }
     }
