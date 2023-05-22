9c4678c0730060feacf631d9e45d2d25a5eb75b2
Vivien Piccin <Vivien.Piccin@cpnv.ch>
2023-05-22 15:57:40 +0200

    Added registered users display

--- resources/views/users/registration.blade.php
+++ resources/views/users/registration.blade.php
@@ -21,4 +21,70 @@
         <tr>
     </tfoot>
 </table>
+<br>
+<table style="width: 100%; border: solid gray 2px;">
+    <thead>
+        <tr>
+            <th colspan="3" style="text-align: center; border: solid gray 2px;">Unregistered users</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($unregisteredUsers as $user)
+        <tr>
+            <td style="border: solid gray 1px;">{{ $user->last_name }}</td>
+            <td style="border: solid gray 1px;">{{ $user->first_name }}</td>
+            <td style="border: solid gray 1px;"><a href="mailto:{{$user->email}}">{{ $user->email }}</a></td>
+        </tr>
+        @endforeach
+    </tbody>
+    <tfoot>
+        <tr>
+            <th colspan="3" style="text-align: center; border: solid gray 2px;"><a href="mailto:?bcc=@foreach($unregisteredUsers as $user){{ $user->email }};@endforeach">Send email to all</a></th>
+        <tr>
+    </tfoot>
+</table>
+<br>
+<table style="width: 100%; border: solid gray 2px;">
+    <thead>
+        <tr>
+            <th colspan="3" style="text-align: center; border: solid gray 2px;">Registered required users</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($requiredRegisteredUsers as $user)
+        <tr>
+            <td style="border: solid gray 1px;">{{ $user->last_name }}</td>
+            <td style="border: solid gray 1px;">{{ $user->first_name }}</td>
+            <td style="border: solid gray 1px;"><a href="mailto:{{$user->email}}">{{ $user->email }}</a></td>
+        </tr>
+        @endforeach
+    </tbody>
+    <tfoot>
+        <tr>
+            <th colspan="3" style="text-align: center; border: solid gray 2px;"><a href="mailto:?bcc=@foreach($requiredRegisteredUsers as $user){{ $user->email }};@endforeach">Send email to all</a></th>
+        <tr>
+    </tfoot>
+</table>
+<br>
+<table style="width: 100%; border: solid gray 2px;">
+    <thead>
+        <tr>
+            <th colspan="3" style="text-align: center; border: solid gray 2px;">Registered users</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($registeredUsers as $user)
+        <tr>
+            <td style="border: solid gray 1px;">{{ $user->last_name }}</td>
+            <td style="border: solid gray 1px;">{{ $user->first_name }}</td>
+            <td style="border: solid gray 1px;"><a href="mailto:{{$user->email}}">{{ $user->email }}</a></td>
+        </tr>
+        @endforeach
+    </tbody>
+    <tfoot>
+        <tr>
+            <th colspan="3" style="text-align: center; border: solid gray 2px;"><a href="mailto:?bcc=@foreach($registeredUsers as $user){{ $user->email }};@endforeach">Send email to all</a></th>
+        <tr>
+    </tfoot>
+</table>
 @stop
